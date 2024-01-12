f7039c644391cc2025e0941dad712aaaf2e4ecd4
Lucas Favaretto <lucasmelofavaretto@hotmail.com>
2024-01-12 16:31:22 -0300

    inclusao te admin, login e pages

--- django/djblog/urls.py
+++ django/djblog/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for djblog project.
+"""djblog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from blog.views import new_post, PostList, PostDetail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),  # NEW
+    path('new/', new_post, name='new_post'),
+    path('', PostList.as_view(), name='index'),
+    path('<slug:slug>/', PostDetail.as_view(), name='detail'),
 ]
